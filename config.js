f8f5422bf8b037bc2a13e94baf9463d0d28fba6a
freze2212 <frezeit@kjc.com>
2026-09-05 19:06:51 +0700

    Update link & telegram for domain gg88uc.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* Config JS - Dynamic Domain Target Link Resolution */
 (function () {
-    var DEFAULT_REDIRECT_URL = "https://www.gg8850.com/?id=914669054";
+    var DEFAULT_REDIRECT_URL = "https://gg8847.com/?id=616150740";
     window.REDIRECT_URL = window.REDIRECT_URL || DEFAULT_REDIRECT_URL;
 
     function getCleanHost() {
